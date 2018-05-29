645c83b5920b29539c5cd39c0bf2a350c5ef33bd
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-29 06:49:11 -0500

    Archivos estáticos

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# app.py
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def hello():
