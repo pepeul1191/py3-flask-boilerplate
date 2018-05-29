cf8c0703f9f01cade7d2ea383f9a899d91c4665a
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-28 23:41:13 -0500

    Añadiendo cabecera a todas la peticiones HTTP

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def hello():
   return "Hello World!"
 
+@app.after_request
+def apply_caching(response):
+  response.headers['Server'] = 'Ubuntu; Werkzeug/0.14.1 Python/3.5.2'
+  return response
+
 if __name__ == '__main__':
   app.config['SESSION_TYPE'] = 'filesystem'
   app.run(debug=True, host='0.0.0.0', port=3000)
